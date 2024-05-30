0be626f4813737210c0b99fb3882a9d2b0492f95
jinbread <jinjae.lee@icloud.com>
2024-05-30 11:51:19 +0900

    Setup main page

---
+++ src/pages/contactList.tsx
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+import ContactListItem from "../components/contactListItem"
+
+const inter = Inter({ subsets: ["latin"] });
+const name = ["Olivia Smith", "Kristoffer Johnsson", "Oscar Karlsson", "Thuy Nguyen", "Trieuvy Luu", "Fanny Carlsson", "Anna Karin"
+]
+
+
+export default function ContactList() {
+  return (
+    <main className="flex items-center justify-center p-12">
+      <div className="grid grid-cols-2 gap-3">
+      {name.map((item, index)=> {
+        return <ContactListItem name={item} key={index} />
+      })}
+      </div>
+    </main>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
 import { Inter } from "next/font/google";
-import ContactListItem from "../components/contactListItem"
+import Link from 'next/link'
+
 
 const inter = Inter({ subsets: ["latin"] });
-const name = ["Olivia Smith", "Kristoffer Johnsson", "Oscar Karlsson", "Thuy Nguyen", "Trieuvy Luu", "Fanny Carlsson", "Anna Karin"
-]
 
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center p-12">
-      <div className="grid grid-cols-2 gap-3">
-      {name.map((item, index)=> {
-        return <ContactListItem name={item} key={index} />
-      })}
-      </div>
+      <Link href="./contactList">Contact List UX</Link>
     </main>
   );
 }
